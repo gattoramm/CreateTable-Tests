80534fea839106edd4cb7c7486312fcdb0a83e18
gattoramm <gattoramm@gmail.com>
2024-03-07 11:32:49 +0300

    Update test_attributes.py

--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -1,9 +1,10 @@
 import pytest
+import app.sqlscripts_generator as sql_generator
+import app.sqlscripts_constructor as sql_constructor
 
 
-@pytest.mark.parametrize('dirname, expected', [
-    ('dir1_fixture', 'expected1'),
-    ('dir2_fixture', 'expected2')])
-def test_directory_command(dirname, expected, request):
-    result = my_package.directory_command(request.getfixturevalue(dirname))
-    assert result == expected
+@pytest.mark.parametrize()
+def test_directory_command():
+    scripts_list = sql_generator.get_sql_scripts_list()
+    map_ttc = sql_constructor.map_type__table_column
+    pass
