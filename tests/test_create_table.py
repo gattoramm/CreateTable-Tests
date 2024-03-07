448a818fd844132b1721fb11149f59ac93ddbe2c
gattoramm <gattoramm@gmail.com>
2024-03-07 10:16:00 +0300

    Update test_create_table.py

--- tests/test_create_table.py
+++ tests/test_create_table.py
@@ -1,19 +1,4 @@
-import random
-
-
-def test_query(db_connection):
-    with db_connection as conn:
+def test_query(init_db):
+    pass
+    with init_db as conn:
         cursor = conn.cursor()
-        str = random.Random(5)
-        print(str)
-        cursor.execute("INSERT INTO users (name) VALUES ('{}')".format(str))
-        conn.commit()
-
-
-def test_query2(db_connection):
-    with db_connection as conn:
-        cursor = conn.cursor()
-        str = random.Random(5)
-        print(str)
-        cursor.execute("INSERT INTO users2 (name) VALUES ('{}')".format(str))
-        conn.commit()
